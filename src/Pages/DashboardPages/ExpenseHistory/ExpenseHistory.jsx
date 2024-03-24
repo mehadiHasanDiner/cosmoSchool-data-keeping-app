155e5fb3853f676984276d802e3ba03152fd2d5a
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-03-24 16:37:47 +0600

    expense history updated

--- src/Pages/DashboardPages/ExpenseHistory/ExpenseHistory.jsx
+++ src/Pages/DashboardPages/ExpenseHistory/ExpenseHistory.jsx
@@ -4,19 +4,36 @@ import useStore from "../../../hooks/useStore";
 
 const ExpenseHistory = () => {
   const [allExpense, setAllExpense] = useState([]);
+  const [storeDetails, setStoreDetails] = useState([]);
   const { mirpurBranch } = useAuth();
   const [, storeItems] = useStore();
 
-  console.log(storeItems);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_URL_KEY}/employeeExpense/${mirpurBranch}`)
       .then((res) => res.json())
       .then((data) => {
         setAllExpense(data);
-        console.log(data);
       });
   }, [mirpurBranch]);
+
+  useEffect(() => {
+    if (storeItems.length > 0 && allExpense.length > 0) {
+      const filteredStoreDetails = [];
+      allExpense.forEach((expense) => {
+        Object.keys(expense).forEach((key) => {
+          const storeDetail = storeItems.find((item) => item._id === key);
+          if (storeDetail) {
+            filteredStoreDetails.push(storeDetail);
+          }
+        });
+      });
+      setStoreDetails(filteredStoreDetails);
+    }
+  }, [storeItems, allExpense]);
+
+  console.log(storeDetails);
+
   return (
     <div>
       <p>Expense History</p>
